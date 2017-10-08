f9c89ed204977d894b83792eaa2a988e9d33e303
Casey He <casey.he@gmail.com>
2017-10-08 17:35:56 -0400

    new commit

--- Assignment2/fsh.c
+++ Assignment2/fsh.c
@@ -10,153 +10,162 @@
 
 #define N_BUF 4096
 
-int refilecheck()
+int refilecheck(char* test_buf, struct stat fileStat, struct stat testStat, struct dirent* de, char* og_buf, char* og_path, char* testPath)
 {
-	int mem = memcmp( *og_buf, *test_buf, N_BUF);
+	int mem = 0; //memcmp( og_buf, test_buf, N_BUF);
 	while (!mem)
 	{
-		// shift the both buff sizes over and do another compare	
-		int s = read(de.d_name, test_buf, N_BUF);
-		int j = read((og_path, og_buf, N_BUF);
+		//cannot read something i didn't open:
+		int q = open( testPath, O_RDONLY);
+		int r = open( og_path, O_RDONLY);
+		int s = read(q, test_buf, N_BUF);
+		int j = read(r, og_buf, N_BUF);
 		if (s < 0)
 		{	
-			fprintf(stderr,"Error reading file %s: %s\n",de.de_name, streerror(errno));
+			fprintf(stderr,"Error reading file %s: %s\n",de->d_name, strerror(errno));
 			exit(-1);
 		}
 		if (j < 0)
 		{	
-		fprintf(stderr,"Error reading file %s: %s\n",og_path, streerror(errno));
+		fprintf(stderr,"Error reading file %s: %s\n",og_path, strerror(errno));
 			exit(-1);
 		}
-		if !(s == j)
-		{
-			//files are different, dont care about the files
-			// set diff true
-		return(0);
+		if (!(s == j))
+		{	//file sizes are different
+			printf("1");
+			return(0);
 		}	
 		if ( s==0 && j ==0)
 		{
-			// we have gotten to the end of both files
-			// and mem still is showing that the files are the same
-			// set diff boolean FALSE
+			// we have gotten to the end of both files and mem sill is showing that the files are the same
 			return (1);
 		}
-		mem = memcmp( *og_buf, *test_buf, N_BUF);
+		mem = memcmp( og_buf, test_buf, N_BUF);
 	}
 	if (mem)
 	{	
 		//the files are different 
-		//set diff TRUE
+		printf("2");
 		return(0);
+
+	}
+	else 
+	{
+		return(1);
 	}
 }	
 
-
-
-int file_traverse( char filename, char pathname)
+void file_traverse(int argc, char** argv, struct stat fileStat, char* og_buf, char* og_path)
 {
 	struct stat testStat;
 	struct dirent *de;
 	DIR *dirp;
-	if (!(dirp = opendir(pathname))
+	if (!(dirp = opendir(argv[2])))
 	{
-		fprintf(stderr,"Can not open directory %s:%s", pathname, strerror(errno));
-		exit (-1);
+		fprintf(stderr,"Can not open directory %s:%s",argv[2], strerror(errno));
+		return;
 	}
-	//so now i opened the dir, must read the directory
+	//so now directory is opened for reading
 	while ( de = readdir(dirp))
 	{
-		char testPath[2048];
-		strcpy(testPath, pathname);
-		strcpy(testPath, "/");
-		strcpy(testPath, de.d_name);
+		char testPath[2048] = {0};
+		strcat(testPath,argv[2]);
+		//strcat(testPath, "/");
+		strcat(testPath, de->d_name);
 
-		int m = stat(testPath, testStat);
+		int m = stat(testPath, &testStat);
 		if (m < 0)
 		{
-			printf(stderr, "Error occurred accessing file information for %s: %s", de.d_name, strerror(errno));
-			exit(-1);
+			fprintf(stderr, "Error occurred accessing file information for %s: %s", de->d_name, strerror(errno));
+			return;
 		}
 		if (testStat.st_mode == S_IFREG || S_IFLNK) // only check for regular files and symlinks
 		{
 			if (testStat.st_size == fileStat.st_size)
+				// the file sizes are the same, check to see if the contents are the same
 			{	
-			// the file sizes are the same, look into the file and see if the contents are the same
-			char* test_buf = malloc(sizeof(char) * N_BUF);
-			int s = read(de.d_name, test_buf, N_BUF);
-			if (s<0)
-			{
-				fprintf(stderr," Error occurred reading file %s: %s", de.d_name, strerror(errno));
-				exit(-1);
-			}
-	/////
-/////////////////////////
-			int recheck = refilecheck(); //TODO missing parameters;
-			if (recheck)
-			{
-				// check if it is a hard link or a duplicate of target or symlink
-				if (testStat.st_nlink == 2);
-					{		//FILE IS A DUPLICATE OF TARGET (NLINK) == 2
-				
+				char* test_buf = malloc(sizeof(char) * N_BUF);
+			
+				int recheck = refilecheck(test_buf, fileStat, testStat, de, og_buf, og_path, testPath); //TODO missing parameters;
+				printf("a");
+				if (recheck)
+				{
+				printf("b");
+					// check if it is a hard link or a duplicate of target or symlink
+					if(fileStat.st_dev == testStat.st_dev && fileStat.st_ino == testStat.st_ino) // inode match
+					{
+						printf("c");
+						//file is a hard link of target
+						printf("%s\t\t HARD LINK TO TARGET", testPath);
 					}
-				else if ( testStat.st_mode == S_IFLINK){
+					else if ( testStat.st_mode == S_IFLNK)
 					{
 						//file is a SYMLINK
+						printf("d");
 						//check if it is a symlink of original file or a simlynk to a hard link or something else
+						//create a buffer, read simlink into the buffer, and then check if it is the same as the path
+						char testContents[2048];
+						int fo = open(testPath, O_RDONLY, 0666);
+						if (fo < 0)
+						{
+							fprintf(stderr, "Error opening file %s: %s",testPath, strerror(errno));
+						}
+						int fr = read( fo, testContents, sizeof(testContents) -1);
+						if (fr < 0)
+						{
+							fprintf( stderr, "error reading file %s: %s", testPath, strerror(errno));
+						}
+						int symlk = memcmp( testContents,&og_path, sizeof(testContents)-1);
+						if (!symlink)
+						{
+							printf("%s\t\t SYMLINK RESOLVES TO TARGET", testPath);
+						}
+						else 
+						{
+							printf("%s\t\t SYMLINK RESOLVES TO DUPLICATE: %s", testPath, testContents);
+						}
 					}
-				else
-					{
-						//file is a hard link of target
+					else
+					{		//FILE IS A DUPLICATE OF TARGET (NLINK) == 2
+							printf("%s\t\t DUPLICATE OF TARGET (NLINK =	%n)\t", testPath, testStat.st_nlink);
 					}
-
-
-	while (s) // now compare the contents of both files buffer by buffer to see if there's any difference in data
-			{
-				int mem = memcmp( *og_buf, *test_buf, N_BUF);
-				if (mem)
-				{
-					// there is a difference in the initial 4096 bytes
-					return;
 				}
-				// check the rest of the file contents to see if the same
-					
-		
+			}
+		}
+		else if ( testStat.st_mode == S_IFDIR) // is a directory
+		{
+			file_traverse(argc, argv,fileStat, og_buf, og_path);
+		}
+	}
+	int cd = closedir(dirp);
+	if (cd < 0 )
+	{
+		fprintf(stderr, "Error occurred closing directory %s: %s", argv[2], strerror(errno));
+		return;
+	}
 }
+
 
 int main(int argc, char** argv)
 {
-
-//parse the input of the terminal as the file to be checked
-//find the file in question and record all the information about it
-//then traverse the rest of the files in the directory going down every path 
-// returning hard links and soft links and similar files and their sizez
-//use opendir closedir readdir
-//also be able to read binart files...(strncmp and memcmp difference?)
-//what ways to disqualify an inode as a potential match?
-////do a size comparison
-////
 	if (argc != 3)
 	{
 		printf("Invalid number of arguments: You entered %d argument(s) instead of 2\n", argc-1);
 		return(-1);
 	}
-	char filename[1024]; 
-	strcpy(filename, argv[1]);
-	char pathname[1024];
-	strcpy(pathname, argv[2]);
 	
-	char OD = opendir(pathname);
+	DIR* OD = opendir(argv[2]);
 	if (OD == NULL)
 	{
-		fprintf(stderr, "Error occurred opening directory %s: %s", pathname, strerror(errno));
+		fprintf(stderr, "Error occurred opening directory %s: %s", argv[2], strerror(errno));
 		exit(-1);
 	}
 	// now the pathname directory is opened		
 	struct stat fileStat;
-	int n = stat(filename, &fileStat);
+	int n = stat(argv[1], &fileStat);
 	if (n <0 )
 	{
-		printf(stderr, "Error occurred accessing file information for %s: %s", filename, strerror(errno));
+		fprintf(stderr, "Error occurred accessing file information for %s: %s", argv[1], strerror(errno));
 		exit(-1);
 	}
 	//load contents into a buffer
@@ -164,16 +173,15 @@ int main(int argc, char** argv)
 
 	// create the path string 
 	char og_path[2048];
-	strcpy(og_path, pathname);
-	strcpy(og_path, "/");
-	strcpy(og_path, filename);
+	strcat(og_path, argv[2]);
+	//strcat(og_path, "/");
+	strcat(og_path, argv[1]);
 
-	int r = read(og_path, og_buf, N_BUF); // first 4096 read into og_buffer		
-
-	// stor information about the key file
-	
-	// call the file_traverse function w/ the current file path
-
-	
+	file_traverse(argc,argv,fileStat, og_buf, og_path);	//call the file traversr
+	int mcd = closedir(OD);
+	if ( mcd < 0 )
+	{
+		fprintf(stderr," Error closing original directory %s: %s", argv[2], strerror(errno));
+	}
 }
 
